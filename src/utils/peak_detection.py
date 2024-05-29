7cfb67e1711610f672c90436cdb2306dc81309ba
Kyunghoon Han (Bonghyun) <kyunghoon.h@gmail.com>
2024-05-29 17:09:09 +0200

    added the minimum aplitude requirements on peak selection

--- src/utils/peak_detection.py
+++ src/utils/peak_detection.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def find_peaks(class_interpolate, window_size=10, threshold=0.01):
+def find_peaks(class_interpolate, window_size=10, threshold=0.01, min_amp=0.0):
         peaks = []
 
         second_deriv = second_derivative(class_interpolate)
@@ -9,8 +9,12 @@ def find_peaks(class_interpolate, window_size=10, threshold=0.01):
             window_second_deriv = second_deriv[i - window_size:i + window_size + 1]
             window              = class_interpolate.y_val[i - window_size:i + window_size + 1]
             
+            bool_amplitude = class_interpolate.y_val[i] > min_amp
+            bool_threshold = second_deriv[i] == np.min(window_second_deriv) and class_interpolate.y_val[i] > np.max(window)-threshold
+
             # Check if the central point is the maximum within its window
-            if second_deriv[i] == np.min(window_second_deriv) and class_interpolate.y_val[i] > np.max(window)-threshold:
+            # and if the amplitude is bigger than the minimum required value
+            if bool_amplitude and bool_threshold:
                 peaks.append(i)
         
         return peaks
--- src/wizardPages/peak_detectionPage.py
+++ src/wizardPages/peak_detectionPage.py
@@ -16,16 +16,20 @@ class PeakDetectionPage(QWizardPage):
         self.peak_indices        = []
         self.window_size         = 10
         self.threshold           = 0.1
+        self.min_amps            = 0.0
         
         # GUI Layouts
         self.layout1                  = QHBoxLayout()
         self.layout2                  = QVBoxLayout()
         # text components
         self.label_window_size        = QLabel()
+        self.minimum_amplitude        = QLabel()
         self.label_threshold          = QLabel()
         self.label_window_size.setText("Window size for peak detection:")
+        self.minimum_amplitude.setText("Minimum amplitude (default : 0.0)")
         self.label_threshold.setText("Threshold (default: 0.05)")
         self.window_size_edit         = QLineEdit("10")
+        self.min_amp_edit             = QLineEdit("0.0")
         self.threshold_edit           = QLineEdit("0.05")
         self.button_run               = QPushButton("Detect Peaks")
         self.button_clear             = QPushButton("Clear plot")
@@ -33,6 +37,7 @@ class PeakDetectionPage(QWizardPage):
         # functions
         self.window_size_edit.textChanged.connect(self.window_size_changes)
         self.threshold_edit.textChanged.connect(self.threshold_changes)
+        self.min_amp_edit.textChanged.connect(self.min_amp_changes)
         self.button_run.clicked.connect(self.run)
         self.button_clear.clicked.connect(self.clear)
         
@@ -50,6 +55,8 @@ class PeakDetectionPage(QWizardPage):
         self.layout1.addLayout(self.layout2)
         self.layout2.addWidget(self.label_window_size)
         self.layout2.addWidget(self.window_size_edit)
+        self.layout2.addWidget(self.minimum_amplitude)
+        self.layout2.addWidget(self.min_amp_edit)
         self.layout2.addWidget(self.label_threshold)
         self.layout2.addWidget(self.threshold_edit)
         self.layout2.addWidget(self.button_run)
@@ -62,7 +69,9 @@ class PeakDetectionPage(QWizardPage):
     
     def run(self):
         self.peak_indices = find_peaks(self.interpolation_class, 
-                                  window_size=self.window_size, threshold=self.threshold)
+                                  window_size=self.window_size,
+                                  threshold=self.threshold,
+                                  min_amp=self.min_amps)
         self.plot_input_data(peak=True)
         
     
@@ -71,6 +80,9 @@ class PeakDetectionPage(QWizardPage):
     
     def threshold_changes(self):
         self.threshold = float(self.threshold_edit.text())
+        
+    def min_amp_changes(self):
+        self.min_amps  = float(self.min_amp_edit.text())
     
     def plot_input_data(self, peak=False):
         """Plot the input data called by the self.read_file function
