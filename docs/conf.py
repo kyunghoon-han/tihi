ca287eb61a71d7a49eecf937d925dcaffb6edd06
Kyunghoon Han (Bonghyun) <kyunghoon.h@gmail.com>
2024-06-27 18:00:57 +0200

    removed the static option on html_static_path

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,8 @@ sys.path.insert(0, os.path.abspath('../src'))
 project = 'Tihi'
 copyright = '2024, Kyunghoon Han'
 author = 'Kyunghoon Han'
-release = '0.0.2'
+release = '0.0.4'
+version = '0.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -22,11 +23,16 @@ release = '0.0.2'
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.ifconfig',
 ]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
+source_suffix = '.rst'
+master_doc = 'index'
+pygments_style = 'sphinx'
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -35,3 +41,4 @@ exclude_patterns = []
 html_theme = 'sphinx_rtd_theme'
 html_static_path = []
 html_favicon = "../src/tihi/icons/64.png"
+htmlhelp_basename = '{}doc'.format(project)
