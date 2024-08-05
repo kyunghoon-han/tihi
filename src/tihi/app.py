6eccfef367e77aa10731588fa704f8e1d4b6e885
kyunghoon-han <kyunghoon.h@gmail.com>
2024-08-05 16:20:41 +0200

    removed the jupyter notebooks

--- src/tihi/app.py
+++ src/tihi/app.py
@@ -34,19 +34,19 @@ class WizardWindow(QMainWindow):
             y_vals (np array): y-values of the signal
         """
         super().__init__()
-        
+
         # initial stuff
         self.approximation  = None
         self.decompositions = []
         self.params         = []
         self.dist_type      = None
-        
+
         self.wizard = MagicWizard(x_vals, y_vals, x_label, y_label, title)
         self.setCentralWidget(self.wizard)
         self.wizard.button(QWizard.FinishButton).clicked.connect(self.closure)
         # Disable the back button on initialization
         self.wizard.button(QWizard.BackButton).hide()
-    
+
     def closure(self):
         self.dist_type = self.wizard.distribution.distribution_type
         if self.dist_type is not None:
@@ -54,7 +54,7 @@ class WizardWindow(QMainWindow):
             self.decompositions = self.wizard.distribution.decompositions
             self.params         = self.wizard.distribution.params
         self.close()
-        
+
 
 class Window(QMainWindow):
     """
@@ -63,12 +63,12 @@ class Window(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Tihi spectral peak finder / analyser")
-        
+
         my_app_id = "tihi.1.0.0"
         # if the OS is Windows...
         if platform.system()=='Windows':
             ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(my_app_id)
-        
+
         icon_path = "src/tihi/logo_small.png"
         icon_obj = QIcon()
         icon_obj.addFile('src/tihi/icons/16.png', QSize(16,16))
@@ -77,7 +77,7 @@ class Window(QMainWindow):
         icon_obj.addFile('src/tihi/icons/128.png', QSize(128,128))
         icon_obj.addFile('src/tihi/icons/256.png', QSize(256,256))
         self.setWindowIcon(icon_obj)
-        
+
         tray = QSystemTrayIcon()
         tray.setIcon(icon_obj)
         tray.setVisible(True)
@@ -88,26 +88,26 @@ class Window(QMainWindow):
         self.y_vals   = None
         self.x_orig   = None    # data for the original signal
         self.y_orig   = None
-        
+
         self.params   = None
-        
+
         # define the buttons
         self.button_load = QPushButton("Load File")
         self.button_load.clicked.connect(self.read_file)
         self.button_load.setToolTip("Load a file where the first column is the x-values and the second column is the y-values")
-        
+
         self.button_run = QPushButton("Run")
         self.button_run.setToolTip("Run the wizard that guides the users to apply the peak detection and fitting algorithms")
         self.button_run.clicked.connect(self.run_wizard)
-        
+
         self.button_save = QPushButton("Save parameters")
         self.button_save.setToolTip("Save the peak information: the parameters for the distributions of all peaks")
         self.button_save.clicked.connect(self.save_parameters)
-        
+
         self.button_normalize = QPushButton("Normalize")
         self.button_normalize.setToolTip("Normalize the signal to sit between 0 and 1")
         self.button_normalize.clicked.connect(self.normalize)
-        
+
         # define the text boxes
         self.textbox_title  = QLineEdit("Plot Title")
         self.textbox_xlabel = QLineEdit("X Label Name")
@@ -115,7 +115,7 @@ class Window(QMainWindow):
         self.textbox_title.textChanged.connect(self.set_title)
         self.textbox_xlabel.textChanged.connect(self.set_xlabel)
         self.textbox_ylabel.textChanged.connect(self.set_ylabel)
-        
+
         # text boxes to set the minimum and maximum
         self.textbox_min = QLineEdit("Min Value")
         self.textbox_max = QLineEdit("Max Value")
@@ -124,17 +124,17 @@ class Window(QMainWindow):
         self.textbox_min.setToolTip("Press Enter to Execute the Changes")
         self.textbox_max.setToolTip("Press Enter to Execute the Changes")
 
-        
+
         # graphing area
         size_ratio     = 4
         self.plotter   = pg.PlotWidget()
         self.plot_item = self.plotter.getPlotItem()
         self.plotter.setBackground('w')
-        
+
         # plotting the variables
         self.line_plots = []
         self.peak_plots = []
-        
+
         # the layouts
         layout1 = QVBoxLayout()                     # The main layout
         layout2 = QHBoxLayout()                     # Layout for the buttons
@@ -160,12 +160,12 @@ class Window(QMainWindow):
         # copyright line
         dev = QLabel("For questions, e-mail the developer at: han_kyunghoon@naver.com -- © Kyunghoon Han")
         layout1.addWidget(dev)
-        
+
         # define the main layout
         main_widget = QWidget()
         main_widget.setLayout(layout1)
         self.setCentralWidget(main_widget)
-    
+
     def set_min_value(self):
         """sets the min x-value of the input signal
         """
@@ -176,12 +176,12 @@ class Window(QMainWindow):
             if x_val > min_val:
                 tmp_x_val.append(x_val)
                 tmp_y_val.append(self.y_vals[i])
-                
+
         self.x_vals = tmp_x_val
         self.y_vals = tmp_y_val
         self.plot_input_data()
-        
-        
+
+
     def set_max_value(self):
         """sets the max x-value of the input signal
         """
@@ -198,9 +198,9 @@ class Window(QMainWindow):
         self.y_vals = tmp_y_val
         self.plot_input_data()
 
-        
-        
-    
+
+
+
     def read_file(self):
         """
             Reads a signal file data in the following format:
@@ -214,7 +214,7 @@ class Window(QMainWindow):
             dir_path,
             'Files (*.csv, *.txt)'
         )
-        
+
         if self.file is None:
             return None
         else:
@@ -242,13 +242,13 @@ class Window(QMainWindow):
             self.set_title()
             self.set_xlabel()
             self.set_ylabel()
-            
-            
+
+
     def normalize(self):
         self.y_vals = self.y_vals - np.min(self.y_vals)
         self.y_vals = self.y_vals / np.max(self.y_vals)
         self.plot_input_data()
-    
+
     def save_parameters(self):
         if self.params is not None:
             print("the parameters being saved: ")
@@ -267,7 +267,7 @@ class Window(QMainWindow):
                     string_out += "Center, Amplitude, Gaussian_Width, Lorentzian_Width\n"
                 else:
                     raise TypeError("The distribution name is not saved properly")
-            
+
                 with open(filename, 'w') as file:
                     file.write(string_out)
                     if self.wiz_window.dist_type == "Gaussian":
@@ -300,10 +300,10 @@ class Window(QMainWindow):
                                 file.write(string_out)
             else:
                 return None
-                        
-                        
-            
-            
+
+
+
+
     """
         Run the Wizard
     """
@@ -314,16 +314,16 @@ class Window(QMainWindow):
         self.params         = []
         self.dist_type      = None
         self.plot_input_data()
-        
+
         # recall the cached data if we want to run the method again
         x_label = self.textbox_xlabel.text()
         y_label = self.textbox_ylabel.text()
         title   = self.textbox_title.text()
-        
+
         self.wiz_window = WizardWindow(self.x_vals, self.y_vals,  x_label=x_label, y_label=y_label, title=title)
         self.wiz_window.show()
         self.wiz_window.wizard.button(QWizard.FinishButton).clicked.connect(self.finish_wizard)
-    
+
     def finish_wizard(self):
         if self.wiz_window.dist_type is not None:
             self.plot_input_data()
@@ -344,7 +344,7 @@ class Window(QMainWindow):
         )
         self.line_plots.append(plot_item)       # list of line plots here
         self.plotter.setContentsMargins(0, 0, 0, 0)
-    
+
     def plot_decompositions(self):
         for dist in self.wiz_window.decompositions:
             self.plotter.plot(
@@ -353,7 +353,7 @@ class Window(QMainWindow):
                 connect="finite",
                 pen=pg.mkPen(color=(0,0,255), width=5)
             )
-    
+
     def plot_approx(self):
         self.plotter.plot(
             self.wiz_window.wizard.distribution.interpolation_class.x_val,
@@ -361,24 +361,23 @@ class Window(QMainWindow):
             connect="finite",
             pen=pg.mkPen(color=(0, 255, 0), width=5)
         )
-    
+
     def set_title(self):
         """Set the title of the plot
         """
         self.plotter.setTitle(title=self.textbox_title.text(), size='20px')
-    
+
     def set_xlabel(self):
         """Set the x-axis label of the plot
         """
         self.plotter.setLabel('bottom', self.textbox_xlabel.text(), size='20px', color='#808080')
-    
+
     def set_ylabel(self):
         """Set the y-axis label of the plot
         """
         self.plotter.setLabel('left', self.textbox_ylabel.text(), size='20px', color='#808080')
-    8
-    
-        
+
+
 def main():
     app = QApplication(sys.argv)
     window = Window()
